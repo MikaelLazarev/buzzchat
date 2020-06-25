9ab9025001287f048faf20cdb41e4019ffe778a6
Neeraj Murarka <njmurarka@gmail.com>
2020-06-25 15:17:12 -0700

    New version

--- mobile/config.ts
+++ mobile/config.ts
@@ -3,9 +3,9 @@
  * Copyright (c) 2020. Mikhail Lazarev
  */
 
-export const BACKEND_ADDR = 'http://192.168.0.48:4000';
+export const BACKEND_ADDR = 'http://buzzchat.bluzelle.com:4000';
 
-export const SSO_ADDR = 'http://192.168.0.48:4000';
+export const SSO_ADDR = 'http://buzzchat.bluzelle.com:4000';
 
 console.disableYellowBox = true;
 
