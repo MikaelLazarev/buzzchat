94dc4266e20296da44455d173be719bc28c259f4
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-07-12 13:38:54 +0300

    Allow http connect

--- mobile/config.ts
+++ mobile/config.ts
@@ -3,9 +3,9 @@
  * Copyright (c) 2020. Mikhail Lazarev
  */
 
-export const BACKEND_ADDR = 'http://192.168.0.47:4000';
+export const BACKEND_ADDR = 'https://buzzzchat.herokuapp.com';
 
-export const SSO_ADDR = 'http://192.168.0.47:4000';
+export const SSO_ADDR = 'https://buzzzchat.herokuapp.com';
 
 console.disableYellowBox = true;
 
