417b39c88ad65d2ae50859d7537d9069b88c71d6
Neeraj Murarka <njmurarka@gmail.com>
2020-07-13 19:33:19 -0700

    Renamed app package name

--- mobile/android/app/src/main/java/com/buzzchat/MainActivity.java
+++ mobile/android/app/src/main/java/com/buzzchat/MainActivity.java
@@ -1,4 +1,4 @@
-package com.buzzchat;
+package com.bluzelle.chat;
 
 import com.facebook.react.ReactActivity;
 
--- mobile/android/app/src/main/java/com/buzzchat/MainApplication.java
+++ mobile/android/app/src/main/java/com/buzzchat/MainApplication.java
@@ -1,4 +1,4 @@
-package com.buzzchat;
+package com.bluzelle.chat;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.buzzchat.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.bluzelle.chat.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
