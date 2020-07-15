5282653e55d149a21a660893cd74bb449072f384
Neeraj Murarka <njmurarka@gmail.com>
2020-07-15 12:49:01 -0700

    New build version for iOS

--- mobile/config.ts
+++ mobile/config.ts
@@ -3,12 +3,13 @@
  * Copyright (c) 2020. Mikhail Lazarev
  */
 
-export const BACKEND_ADDR = 'http://192.168.0.47:4000';
-export const SSO_ADDR = 'http://192.168.0.47:4000';
+export const BACKEND_ADDR = 'http://buzzchat.bluzelle.com:4000';
 
-console.disableYellowBox = true;
+export const SSO_ADDR = 'http://buzzchat.bluzelle.com:4000';
 
 /*
+console.disableYellowBox = true;
+
 export const BACKEND_ADDR = 'https://buzzzchat.herokuapp.com';
 
 export const SSO_ADDR = 'https://buzzzchat.herokuapp.com';
